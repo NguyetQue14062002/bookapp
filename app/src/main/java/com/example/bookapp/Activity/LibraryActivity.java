7319622e3fa0e36392daddb1191a22d566d0b920
NguyetQue14062002 <20110706@student.hcmute.edu.vn>
2023-05-05 21:44:44 +0700

    tablayout

--- app/src/main/java/com/example/bookapp/Activity/LibraryActivity.java
+++ app/src/main/java/com/example/bookapp/Activity/LibraryActivity.java
@@ -16,14 +16,15 @@ public class LibraryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_library);
+        setContentView(R.layout.fragment_library);
 
 
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager= findViewById(R.id.view_pager);
 
         ViewLibraryAdapter viewLibraryAdapter = new ViewLibraryAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         viewPager.setAdapter(viewLibraryAdapter);
         tabLayout.setupWithViewPager(viewPager);
-
     }
 
 }
--- app/src/main/java/com/example/bookapp/Fragment/LibraryFragment.java
+++ app/src/main/java/com/example/bookapp/Fragment/LibraryFragment.java
@@ -1,5 +1,6 @@
 package com.example.bookapp.Fragment;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.bookapp.Activity.LibraryActivity;
 import com.example.bookapp.R;
 
 /**
